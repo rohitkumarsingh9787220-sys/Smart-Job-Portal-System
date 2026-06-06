efe6b5f3c3eddf95389f6cbe20ed6395cf91881b
rohitkumarsingh9787220-sys <rohitkumarsingh9787220@gmail.com>
2026-06-06 17:24:40 +0530

    Update main menu system

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println(" SMART JOB PORTAL SYSTEM ");
-        System.out.println("=================================");
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("=== SMART JOB PORTAL ===");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("3. View Jobs");
+
+        int choice = sc.nextInt();
+
+        System.out.println("You selected: " + choice);
     }
 }

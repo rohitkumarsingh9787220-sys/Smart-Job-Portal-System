069ae8c4209e6d6a5a1838137baedbace9791d24
rohitkumarsingh9787220-sys <rohitkumarsingh9787220@gmail.com>
2026-06-06 21:06:46 +0530

    Improve DBConnection configuration

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,5 +1,13 @@
 public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/jobportal";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+
     public static void connect() {
-        System.out.println("Database Connected");
+
+        System.out.println("Connecting to Database...");
+        System.out.println("URL: " + URL);
+        System.out.println("Database Connected Successfully");
     }
 }

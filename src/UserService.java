f197faf3bfdda7e55cb6d438eccc2863db267cbe
rohitkumarsingh9787220-sys <rohitkumarsingh9787220@gmail.com>
2026-06-06 17:09:51 +0530

    Add  UserService model

---
+++ src/UserService.java
@@ -0,0 +1,10 @@
+public class UserService {
+
+    public void registerUser() {
+        System.out.println("User Registered");
+    }
+
+    public void loginUser() {
+        System.out.println("User Logged In");
+    }
+}

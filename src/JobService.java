273fbb188161762ea59a6f4f3963c6ad00a8feab
rohitkumarsingh9787220-sys <rohitkumarsingh9787220@gmail.com>
2026-06-06 21:14:04 +0530

    Add search and application logic in JobService

--- src/JobService.java
+++ src/JobService.java
@@ -2,17 +2,27 @@ public class JobService {
 
     public void viewJobs() {
 
+        System.out.println("===== AVAILABLE JOBS =====");
         System.out.println("1. IBM - Associate System Engineer");
-        System.out.println("2. Microland - Associate Engineer");
-        System.out.println("3. HCLTech - Graduate Engineer Trainee");
-
+        System.out.println("2. Microland - Graduate Engineer Trainee");
+        System.out.println("3. HCLTech - Software Engineer");
     }
 
     public void searchJobs(String skill) {
-        System.out.println("Searching jobs for: " + skill);
+
+        System.out.println("Searching jobs for skill: " + skill);
+
+        if (skill.equalsIgnoreCase("Java")) {
+            System.out.println("Found: IBM ASE");
+            System.out.println("Found: HCLTech Software Engineer");
+        } else {
+            System.out.println("No matching jobs found.");
+        }
     }
 
     public void applyJob(int jobId) {
-        System.out.println("Applied for Job ID: " + jobId);
+
+        System.out.println("Application Submitted Successfully");
+        System.out.println("Applied Job ID: " + jobId);
     }
 }

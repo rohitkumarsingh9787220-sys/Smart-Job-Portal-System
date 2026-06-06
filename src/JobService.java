50c8105b32111c9dd9b7e4654f4d74e27bb1f208
rohitkumarsingh9787220-sys <rohitkumarsingh9787220@gmail.com>
2026-06-06 17:27:25 +0530

    Add job listing and apply feature

--- src/JobService.java
+++ src/JobService.java
@@ -1,6 +1,18 @@
 public class JobService {
 
     public void viewJobs() {
-        System.out.println("Viewing Jobs");
+
+        System.out.println("1. IBM - Associate System Engineer");
+        System.out.println("2. Microland - Associate Engineer");
+        System.out.println("3. HCLTech - Graduate Engineer Trainee");
+
+    }
+
+    public void searchJobs(String skill) {
+        System.out.println("Searching jobs for: " + skill);
+    }
+
+    public void applyJob(int jobId) {
+        System.out.println("Applied for Job ID: " + jobId);
     }
 }

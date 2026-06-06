01b855dcc005f03bd1ff6e4bf7f8b6bee6d823d4
rohitkumarsingh9787220-sys <rohitkumarsingh9787220@gmail.com>
2026-06-06 20:59:22 +0530

    Improve AdminService functionality

--- src/AdminService.java
+++ src/AdminService.java
@@ -1,6 +1,14 @@
 public class AdminService {
 
     public void addJob() {
-        System.out.println("Job Added");
+        System.out.println("New Job Added Successfully");
+    }
+
+    public void removeJob(int jobId) {
+        System.out.println("Job Removed: " + jobId);
+    }
+
+    public void viewApplications() {
+        System.out.println("Displaying All Applications");
     }
 }
